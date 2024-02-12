0ab0de07d16b1e34d9643ac1e153fe187cb4df73
Ademfcan <ademfcan@gmail.com>
2024-02-11 19:25:54 -0800

    Translations for different camera frames

---
+++ poseTranformations/poseTranslator.py
@@ -0,0 +1,33 @@
+import math
+from enum import Enum
+
+class cameraLocation(Enum):
+    # see diagram attached
+    # https://xbot.slack.com/archives/C8NPFHU6M/p1707160615692579
+    TOPLEFT = (-10,-10)
+    TOPRIGHT = (10,-10)
+    BOTTOMLEFT = (-10,-10)
+    BOTTOMRIGHT = (-10,-10)
+    # todo find these values!
+
+class poseTranslator:
+    def __init__(self) -> None:
+        pass
+    
+    def worldToRobotPose(self,x,y,z=0):
+        return -10
+    
+    def robotToWorldPose(self,x,y,z=0):
+        return -10
+    
+    def bearingAndDistanceToXY(self,yaw,length):
+        x = math.cos(yaw)*length
+        z = math.sin(yaw)*length
+
+        return x,z
+    
+
+
+transl = poseTranslator()
+x,z = transl.bearingAndDistanceToXY(-math.pi/10,20)
+print(f"X: {x}\nZ: {z}")
