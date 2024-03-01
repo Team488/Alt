60073977b423e63a81cd86fb57db8f470e4ddc34
johnylamw <johnylamw@gmail.com>
2024-02-29 23:10:21 -0800

    wip fixing rounding

--- python-test/main.py
+++ python-test/main.py
@@ -1,6 +1,41 @@
 import probmap
+import random
 
-fieldMap = probmap.ProbMap(2000, 1000, 5)
-fieldMap.display_map()
-fieldMap.add_detection(300, 150, 450, 450, 0.5)
-fieldMap.display_map()
+def main():
+    fieldMap = probmap.ProbMap(2000, 1000, 5)
+    fieldMap.display_map()
+    fieldMap.add_detection(300, 150, 450, 450, 0.5)
+    fieldMap.display_map()    
+    
+    # test_randomization_ranges(488)
+
+
+# randomizes values for stress testing algorithm
+def test_randomization_ranges(seed):
+    random.seed(seed)
+
+    fieldMap = probmap.ProbMap(2000, 1000, 5)
+    fieldMap.display_map()
+    obj_size = 1
+    failedsizes = []
+    for i in range(300):
+        x = random.randrange(0, 2000)
+        y = random.randrange(0, 1000)
+        # obj_size = random.randrange(100, 200)
+        confidence = random.randrange(50, 100, 1)/100
+        obj_size += 1
+        print(x, y, obj_size, confidence)
+        fieldMap.add_detection(x, y, obj_size, obj_size, confidence)
+
+        # try:
+            # fieldMap.add_detection(x, y, obj_size, obj_size, confidence)
+        # except:
+            # failedsizes.append(obj_size)
+    fieldMap.display_map()
+    print(failedsizes)
+    
+    
+if __name__ == "__main__":
+    main()
+    
+    
--- python-test/probmap.py
+++ python-test/probmap.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
-
+import math
+from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_DOWN
 # This whole thing is axis aligned for speed, but that may not work great
 class ProbMap:
     def __init__(self, x, y, resolution):
@@ -14,9 +15,10 @@ class ProbMap:
     def add_detection(self, x, y, obj_x, obj_y, prob):
         # Given the object size, spread the detection out by stddevs of probabilities
         # Consider making the blobs themselves larger or smaller based on probabilities instead?
-        gauss_x, gauss_y = np.meshgrid(np.linspace(-3.0*(1.0-prob), 3.0*(1.0-prob), obj_x), np.linspace(-3.0*(1.0-prob), 3.0*(1.0-prob), obj_y))
-        #gauss_x, gauss_y = np.meshgrid(np.linspace(-2.5, 2.5, obj_x), np.linspace(-2.5, 2.5, obj_y))
-
+        # gauss_x, gauss_y = np.meshgrid(np.linspace(-3.0*(1.0-prob), 3.0*(1.0-prob), obj_x), np.linspace(-3.0*(1.0-prob), 3.0*(1.0-prob), obj_y))
+        gauss_x, gauss_y = np.meshgrid(np.linspace(-2.5, 2.5, obj_x), np.linspace(-2.5, 2.5, obj_y))
+        
+        print("gauss_x", gauss_x, "gauss_y", gauss_y)
         gaussian_blob = np.exp(-0.5 * (gauss_x**2 + gauss_y**2))
 
         print('\n' + 'gaussian_blob before: ')
@@ -27,25 +29,37 @@ class ProbMap:
         print(gaussian_blob)
 
         blob_height, blob_width = gaussian_blob.shape[0:2]
+        blob_height = Decimal(blob_height)
         print('\n' + ' gaussian size: ' + str(blob_height) + ', ' + str(blob_width))
-        blob_left_edge_loc = round(x - ((blob_width) * 0.5))
-        blob_right_edge_loc = round(x + ((blob_width) * 0.5))
-        blob_top_edge_loc = round(y - ((blob_height) * 0.5))
-        blob_bottom_edge_loc = round(y + ((blob_height) * 0.5))
+
+        # NOT WORKING.
+        precision = Decimal('1.')
+        blob_left_edge_loc = int((x - (blob_width * Decimal('0.5'))).quantize(precision, rounding=ROUND_HALF_UP))
+        blob_right_edge_loc = int((x + (blob_width * Decimal('0.5'))).quantize(precision, rounding=ROUND_HALF_UP))
+
+        blob_top_edge_loc = int((y - (blob_height * Decimal('0.5'))).quantize(precision, rounding=ROUND_HALF_UP))
+        blob_bottom_edge_loc = int((y + (blob_height * Decimal('0.5'))).quantize(precision, rounding=ROUND_HALF_UP))
+
         # Trimming functions to make sure we don't overflow
+        print("left + right", blob_left_edge_loc, blob_right_edge_loc)
+        print("top + bottom", blob_top_edge_loc, blob_bottom_edge_loc)
         if blob_left_edge_loc < 0:
+            print("left edge out of bounds")
             np.delete(gaussian_blob, np.arange(0, 0-blob_left_edge_loc), axis=1)
             blob_width+=blob_left_edge_loc
             blob_left_edge_loc = 0
         if blob_right_edge_loc > self.size_x:
+            print("right edge out of bounds")
             np.delete(gaussian_blob, np.arange(blob_width-(blob_right_edge_loc-self.size_x), blob_width), axis=1)
             blob_width-=(blob_right_edge_loc-self.size_x)
             blob_right_edge_loc = self.size_x
         if blob_top_edge_loc < 0:
+            print("top edge out of bounds")
             np.delete(gaussian_blob, np.arange(0, 0-blob_top_edge_loc), axis=0)
             blob_height+=blob_top_edge_loc
             blob_top_edge_loc = 0
         if blob_bottom_edge_loc > self.size_y:
+            print("bottom edge out of bounds")
             np.delete(gaussian_blob, np.arange(blob_height-(blob_bottom_edge_loc-self.size_y), blob_height), axis = 0)
             blob_height-=(blob_bottom_edge_loc-self.size_y)
             blob_bottom_edge_loc = self.size_y
@@ -59,18 +73,26 @@ class ProbMap:
         blob_height, blob_width = gaussian_blob.shape[0:2]
 
         print('\n' + ' gaussian size: ' + str(blob_height) + ', ' + str(blob_width))
-        self.probmap[blob_top_edge_loc:blob_bottom_edge_loc, blob_left_edge_loc:blob_right_edge_loc] += gaussian_blob
+        # self.probmap[blob_top_edge_loc:adjusted_blob_bottom_edge_loc, blob_left_edge_loc:adjusted_blob_right_edge_loc] += gaussian_blob                
+        
+        print("x", blob_top_edge_loc, blob_bottom_edge_loc, (blob_bottom_edge_loc - blob_top_edge_loc))
+        print("y", blob_left_edge_loc, blob_right_edge_loc, (blob_right_edge_loc - blob_left_edge_loc))
+        print("prob map actual shape", self.probmap.shape)
+        print("prob map shape", self.probmap[blob_top_edge_loc:blob_bottom_edge_loc, blob_left_edge_loc:blob_right_edge_loc].shape)
+        print("prob map x", self.probmap[blob_top_edge_loc:blob_bottom_edge_loc].shape)
+        print("prob map y", self.probmap[blob_left_edge_loc:blob_right_edge_loc].shape)
+        self.probmap[blob_left_edge_loc:blob_right_edge_loc, blob_top_edge_loc:blob_bottom_edge_loc] += gaussian_blob
     
     def display_map(self):
-            heatmap = np.copy(self.probmap)
-            heatmap = heatmap * 255.0
-            heatmap = np.clip(heatmap, a_min=0.0, a_max=255.0)
-            heatmap = np.rint(heatmap).astype(np.uint8)
-            heatmap = np.where(heatmap > 255, 255, heatmap).astype(np.uint8)
+        heatmap = np.copy(self.probmap)
+        heatmap = heatmap * 255.0
+        heatmap = np.clip(heatmap, a_min=0.0, a_max=255.0)
+        heatmap = np.rint(heatmap).astype(np.uint8)
+        heatmap = np.where(heatmap > 255, 255, heatmap).astype(np.uint8)
+        cv2.imshow('heatmap', heatmap)
+        cv2.waitKey()
 
-            cv2.imshow('heatmap', heatmap)
-            cv2.waitKey()
 
-
+# if you make it smaller
 
 
