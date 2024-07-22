0e1aeae7a2142775a0910f877808d749ebf84524
Ademfcan <ademfcan@gmail.com>
2024-07-22 04:54:34 -0700

    fixed probmap issue where dimensions were misaligned, adding methods to get highest prob, robots and objects get own maps, small demo + other small things

--- python-test/main.py
+++ python-test/main.py
@@ -1,3 +1,4 @@
+import time
 import probmap
 import random
 import traceback
@@ -7,44 +8,94 @@ import struct
 import ntcore as nt
 
 def main():
-    fieldMap = probmap.ProbMap(1654, 821, 1) #Width x Height at 1 cm resolution
-    
-    #Initialize Network Tables
-    instance = nt.NetworkTableInstance.getDefault()
-    instance.startClient4("Probability Map")
-    instance.setServerTeam(488)
-    table = instance.getTable("AdvantageKit")
-    writeTable = instance.getTable("ProbabilityMap").getSubTable("NDAR")
-    poseTable = table.getSubTable("RealOutputs").getSubTable("PoseSubsystem")
+    # fieldMap = probmap.ProbMap(1654, 821, 1) #Width x Height at 1 cm resolution
+    test_run()
+    # go_inorder_top_bottom_left_right()
 
-    while(1):
-        pose_bytes = poseTable.getValue("RobotPose","empty")
-        robotX, robotY, robotR = struct.unpack('ddd',pose_bytes)
-        print(f"X: {robotX}, Y: {robotY}, R: {robotR}")
+
+
+
+
+
+    #Initialize Network Tables
+    # instance = nt.NetworkTableInstance.getDefault()
+    # instance.startClient4("Probability Map")
+    # instance.setServerTeam(488)
+    # table = instance.getTable("AdvantageKit")
+    # writeTable = instance.getTable("ProbabilityMap").getSubTable("NDAR")
+    # poseTable = table.getSubTable("RealOutputs").getSubTable("PoseSubsystem")
+
+    # while(1):
+    #     pose_bytes = poseTable.getValue("RobotPose","empty")
+    #     robotX, robotY, robotR = struct.unpack('ddd',pose_bytes)
+    #     print(f"X: {robotX}, Y: {robotY}, R: {robotR}")
 
 
 
 # randomizes values for stress testing algorithm
 def test_run():
-    fieldMap = probmap.ProbMap(1654, 821, 1) #Width x Height at 1 cm resolution
-    fieldMap.display_map()
-    for i in range(200):
+    # test sizes all in cm
+    robotSizeX = 71 # using max dimensions for this of 28 inch
+    robotSizeY = 96 # using max dimensions for this of 38 inch
+
+    objSize = 35 # using notes from last year with a outside diameter of 14 inch
+    fieldX = 2743 # roughly 90 ft
+    fieldY = 1676 # roughly 55 ft
+
+    # axis aligned so robot detections will be need to be adjusted for accuracy
+    fieldMap = probmap.ProbMap(fieldX, fieldY, 1,objSize,objSize,robotSizeX,robotSizeY) #Width x Height at 1 cm resolution
+    fieldMap.display_maps()
+    for i in range(20000):
         # fieldMap.add_detection(300, 150, 250, 250, 0.5)
-        fieldMap.smooth()
-        test_randomization_ranges(fieldMap, fieldMap.get_shape()[0], fieldMap.get_shape()[1])
-        fieldMap.display_map()
+        fieldMap.disspateOverTime()
+        test_randomization_ranges(fieldMap, int(fieldMap.get_shape()[0]), int(fieldMap.get_shape()[1]))
+        if(i % 2 == 0):
+            fieldMap.disspateOverTime()
+        fieldMap.display_maps()
         # fieldMap.clear_map()
 
-def test_randomization_ranges(map, width, height):
-    for i in range(1):
-        x = random.randrange(0, width)
-        y = random.randrange(0, height)
-        obj_size = 36*6 #size*total potential STD #random.randrange(36, 36) 
-        confidence = random.randrange(65, 95, 1)/100 # generates a confidence threshold between 0.65 - 0.95
-        try:
-            map.add_detection(x, y, obj_size, obj_size, confidence)
-        except Exception:
-            traceback.print_exc()
+def test_randomization_ranges(map : probmap, width, height):
+    #for i in range(1):
+    x = random.randrange(0, width)
+    y = random.randrange(0, height)
+    # obj_size = 36*6 #size*total potential STD #random.randrange(36, 36) 
+    confidence = random.randrange(65, 95, 1)/100 # generates a confidence threshold between 0.65 - 0.95
+    typeRand = random.random() # 50% chance robot / 50% chance object
+    try:
+        if typeRand >= .50:
+            map.addDetectedRobot(x,y,confidence)
+        else:
+            map.addDetectedGameObject(x,y,confidence)
+    except Exception:
+        traceback.print_exc()
+
+def go_inorder_top_bottom_left_right():
+    #for i in range(1):
+    robotSizeX = 71 # using max dimensions for this of 28 inch
+    robotSizeY = 96 # using max dimensions for this of 38 inch
+
+    objSize = 35 # using notes from last year with a outside diameter of 14 inch
+    fieldX = 2743 # roughly 90 ft
+    fieldY = 1676 # roughly 55 ft
+
+    # axis aligned so robot detections will be need to be adjusted for accuracy
+    fieldMap = probmap.ProbMap(fieldX, fieldY, 1,objSize,objSize,robotSizeX,robotSizeY) #Width x Height at 1 cm resolution
+    fieldMap.display_maps()
+    for j in range(100,fieldY-100,100):
+        for i in range(100,fieldX-100,100):
+            # obj_size = 36*6 #size*total potential STD #random.randrange(36, 36) 
+            confidence = random.randrange(65, 95, 1)/100 # generates a confidence threshold between 0.65 - 0.95
+            typeRand = random.random() # 50% chance robot / 50% chance object
+            try:
+                fieldMap.addDetectedRobot(i,j,confidence)
+                fieldMap.addDetectedGameObject(i,j,confidence)
+            except Exception:
+                traceback.print_exc()
+            time.sleep(1)
+            fieldMap.display_maps()
+
+
+
 
 if __name__ == "__main__":
     main()
--- python-test/probmap.py
+++ python-test/probmap.py
@@ -4,15 +4,27 @@ import math
 from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_DOWN, ROUND_FLOOR, ROUND_CEILING
 # This whole thing is axis aligned for speed, but that may not work great
 class ProbMap:
-    def __init__(self, x, y, resolution):
+    def __init__(self, x, y, resolution,gameObjectX,gameObjectY,robotX,robotY,sigma = 1):
         self.size_x = x
         self.size_y = y
+        self.gameObjectX = gameObjectX;
+        self.gameObjectY = gameObjectY;
+        self.robotX = robotX;
+        self.robotY = robotY;
+        self.sigma = sigma # dissipation rate for gaussian blur
         self.resolution = resolution
+        self.isfirstShow = True # for creating named windows
         # create a blank probability map
-        self.probmap = np.zeros((self.size_x, self.size_y), dtype=np.float64)
+        self.probmapGameObj = np.zeros((self.size_x, self.size_y), dtype=np.float64)
+        self.probmapRobots = np.zeros((self.size_x, self.size_y), dtype=np.float64)
 
     #After testing speed, see if we need some sort of hashmap to detection patches
-    def add_detection(self, x, y, obj_x, obj_y, prob):
+    #We could add the center of detections to the hashmap, then on every smooth cycle we traverse each patch in the map and see if the probability has dissipated to zero, if so then we remove from map
+    def __add_detection(self,probmap, x, y, obj_x, obj_y, prob):
+        # not perfect workaround, but transpose fix leads to x and y values being flipped, we can get by this by just flipping before putting in to map
+        tmp = x
+        x = y
+        y = tmp
         print("confidence", prob)
         # Given the object size, spread the detection out by stddevs of probabilities
         # Consider making the blobs themselves larger or smaller based on probabilities instead?
@@ -23,6 +35,7 @@ class ProbMap:
         
         # print("gauss_x", gauss_x, "gauss_y", gauss_y)
         gaussian_blob = np.exp(-0.5 * (gauss_x**2 + gauss_y**2) / sigma**2)
+
 
         # print('\n' + 'gaussian_bQlob before: ')
         # print(gaussian_blob.dtype)
@@ -44,15 +57,18 @@ class ProbMap:
         # print("before trimming left + right", blob_left_edge_loc, blob_right_edge_loc)
         # print("before trimming + bottom", blob_top_edge_loc, blob_bottom_edge_loc)
 
+        # flip shape, this is what was causing issues when trying to add the blob to the probmap
+        gaussian_blob = np.transpose(gaussian_blob)
+
         # Trimming functions to make sure we don't overflow        
         if blob_left_edge_loc < 0:
             # print("left edge out of bounds")
-            gaussian_blob = gaussian_blob[-blob_left_edge_loc:, :]
+            gaussian_blob = gaussian_blob[-blob_left_edge_loc:,:]
             blob_left_edge_loc = 0
 
         if blob_right_edge_loc > self.size_x:
             # print("right edge out of bounds")
-            gaussian_blob = gaussian_blob[:-(blob_right_edge_loc - self.size_x), :]
+            gaussian_blob = gaussian_blob[:-(blob_right_edge_loc - self.size_x),:]
             blob_right_edge_loc = self.size_x
 
         if blob_top_edge_loc < 0:
@@ -62,39 +78,109 @@ class ProbMap:
 
         if blob_bottom_edge_loc > self.size_y:
             # print("bottom edge out of bounds")
-            gaussian_blob = gaussian_blob[:, :-(blob_bottom_edge_loc - self.size_y)]
+            gaussian_blob = gaussian_blob[:,:-(blob_bottom_edge_loc - self.size_y)]
             blob_bottom_edge_loc = self.size_y
             
 
         gaussian_blob = gaussian_blob.astype(np.float64)
-        blob_height, blob_width = gaussian_blob.shape[0:2]
-        # print('\n' + ' gaussian size: ' + str(blob_height) + ', ' + str(blob_width))
-# 
+        # blob_height, blob_width = gaussian_blob.shape[0:2]
+        # print('\n' + 'gaussian size: ' + str(blob_height) + ', ' + str(blob_width))
+
+        
         # print("gaussian x edges", blob_left_edge_loc, blob_right_edge_loc, "diff:", (blob_right_edge_loc - blob_left_edge_loc))        
         # print("gaussian y edges", blob_top_edge_loc, blob_bottom_edge_loc, "diff:", (blob_bottom_edge_loc - blob_top_edge_loc))
-        # print("prob map actual shape", self.probmap.shape)
-        # print("prob map shape", self.probmap[blob_top_edge_loc:blob_bottom_edge_loc, blob_left_edge_loc:blob_right_edge_loc].shape)
-        # print("prob map x", self.probmap[blob_top_edge_loc:blob_bottom_edge_loc].shape)
-        # print("prob map y", self.probmap[blob_left_edge_loc:blob_right_edge_loc].shape)
-        self.probmap[blob_left_edge_loc:blob_right_edge_loc, blob_top_edge_loc:blob_bottom_edge_loc] += gaussian_blob
+        # print("prob map actual shape", probmap.shape)
+        # print("prob map shape", probmap[blob_left_edge_loc:blob_right_edge_loc,blob_top_edge_loc:blob_bottom_edge_loc].shape)
+        # # print("test", probmap[self.size_x-1:, self.size_y-1:].shape)
+        # print("prob map x", probmap[blob_top_edge_loc:blob_bottom_edge_loc].shape)
+        # print("prob map y", probmap[blob_left_edge_loc:blob_right_edge_loc].shape)
+
+        # slicing 
+        probmap[blob_left_edge_loc:blob_right_edge_loc,blob_top_edge_loc:blob_bottom_edge_loc] += gaussian_blob
     
-    def display_map(self):
-        heatmap = np.copy(self.probmap)
+    def addDetectedGameObject(self,x,y,prob):
+        self.__add_detection(self.probmapGameObj,x,y,self.gameObjectX,self.gameObjectX,prob);
+
+
+
+    def addDetectedRobot(self,x,y,prob):
+        self.__add_detection(self.probmapRobots,x,y,self.robotX,self.robotY,prob);
+    
+    def addCustomObjectDetection(self,x,y,objX,objY,prob):
+        self.__add_detection(self.probmapGameObj,x,y,objX,objY,prob);
+
+
+    
+    
+    def __display_map(self,probmap,name : str):
+        heatmap = np.copy(probmap)
         heatmap = heatmap * 255.0
         heatmap = np.clip(heatmap, a_min=0.0, a_max=255.0)
         heatmap = np.rint(heatmap).astype(np.uint8)
         heatmap = np.where(heatmap > 255, 255, heatmap).astype(np.uint8)
-        cv2.imshow('heatmap', heatmap)
+        cv2.imshow(name, heatmap)
         cv2.waitKey(10)
-        
-    def clear_map(self):
-        self.probmap = np.zeros((self.size_x, self.size_y), dtype=np.float64)
     
+    def display_maps(self):
+        if(self.isfirstShow):
+            self.isfirstShow = False
+            cv2.namedWindow("GameObj Map")
+            cv2.namedWindow("Robot Map")
+        self.__display_map(self.probmapGameObj,"GameObj Map")
+        self.__display_map(self.probmapRobots,"Robot Map")
+
+    def createDisplayClickCallbacks(self,func):
+        if(not self.isfirstShow):
+            cv2.setMouseCallback("GameObj Map",func)
+            cv2.setMouseCallback("Robot Map",func)
+        else:
+            print("named windows not setup yet!")
+    
+    def __getHighest(self,probmap):
+        # for now just traversing the array manually but the hashmap idea sounds very powerfull
+        flat_index = np.argmax(probmap)
+        # Convert the flattened index to 2D coordinates
+
+        # y,x coords given so flip
+        coordinates = np.unravel_index(flat_index, probmap.shape)
+        return (coordinates[1],coordinates[0])
+    
+    
+    def getHighestGameObject(self) -> tuple[int,int]:
+        return self.__getHighest(self.probmapGameObj)
+
+    def getHighestRobot(self) -> tuple[int,int]:
+        return self.__getHighest(self.probmapRobots)
+
+
+    
+    def clear_maps(self):
+        self.probmapGameObj = np.zeros((self.size_x, self.size_y), dtype=np.float64)
+        self.probmapRobots = np.zeros((self.size_x, self.size_y), dtype=np.float64)
+
     def get_shape(self):
-        return np.shape(self.probmap)
+        # both maps have same shape
+        return np.shape(self.probmapGameObj)
     
-    def smooth(self):
-        kernel = np.array([0.23, 0.5, 0.23]) # Here you would insert your actual kernel of any size
-        self.probmap = np.apply_along_axis(lambda x: np.convolve(x, kernel, mode='same'), 0, self.probmap)
-        self.probmap = np.apply_along_axis(lambda y: np.convolve(y, kernel, mode='same'), 1, self.probmap)
+    def __smooth(self,probmap):
+        # kernel = np.array([0.23, 0.5, 0.23]) # Here you would insert your actual kernel of any size
+        # probmap = np.apply_along_axis(lambda x: np.convolve(x, kernel, mode='same'), 0, probmap)
+        # probmap = np.apply_along_axis(lambda y: np.convolve(y, kernel, mode='same'), 1, probmap)
+
+        # trying gaussian blur
+        # kernel_size = (35, 35)  # (width, height)
+        # probmap = cv2.GaussianBlur(probmap,kernel_size,self.sigma)
+        # return probmap
+
+        # maybe exponential decay will represent time dependent changes better
+        decayFac = .88
+        return probmap * decayFac
+
+    # thinking of adding some sort of time parameter here to scale the smoothing based on how much time has passed
+    def disspateOverTime(self):
+        self.probmapGameObj = self.__smooth(self.probmapGameObj)
+        self.probmapRobots = self.__smooth(self.probmapRobots)
+
+
+
         
