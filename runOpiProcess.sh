00c668b82ea146f371282db70fd72591927e57fe
Adem <ademfcan@gmail.com>
2024-10-26 00:54:17 -0700

    services

--- runOpiProcess.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash+
-pyenv activate xbot-3.9
-python centralOrangePiProcess.py --sendframe False
--- runOpiProcessUnrestricted.sh
+++ runOpiProcessUnrestricted.sh
@@ -1,8 +1,10 @@
-#! /bin/bash
+#!/bin/bash+
+export PATH=$PATH:/home/pi/.pyenv/bin
 if [[ -x $(command -v pyenv) ]]; then
     eval "$(pyenv init --path)"
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
 fi
+
 pyenv activate xbot-3.9
-python centralMainProcess.py
+python centralOrangePiProcess.py --sendframe False
