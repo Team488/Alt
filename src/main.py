345f1086ad0a868e08fc5a615a28c84e9a60af39
Agent Smith <488matrix@gmail.com>
2025-01-30 22:50:32 -0800

    removing temp ip for main testing

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 from Core.Neo import Neo
 from Core.Agents import AgentExample
 from Core.Orders import OrderExample
+
+from JXTABLES.TempConnectionManager import TempConnectionManager as tcm
+
+# removes the temp ip for testing in main
+tcm.invalidate()
+
 n = Neo()
 n.wakeAgent(AgentExample)
-n.addOrderTrigger("trigger",OrderExample)
+n.addOrderTrigger("trigger", OrderExample)
 n.shutDownOnAgentFinished()
 n.waitForAgentFinished()
