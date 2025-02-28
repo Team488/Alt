a0e41d81d410d81f40dfe5bbb97cf1be837019fe
Ademfcan <ademfcan@gmail.com>
2025-02-28 04:04:58 -0800

    add req and fix naming flip

--- src/inference.py
+++ src/inference.py
@@ -1,9 +1,10 @@
 from Core.Agents.Abstract.InferenceAgent import InferenceAgentPartial
 from Core.Neo import Neo
-from tools.Constants import InferenceMode, CameraIntrinsicsPredefined
+from tools.Constants import InferenceMode, CameraIntrinsicsPredefined, CommonVideos
+from Captures import FileCapture
 
 agent = InferenceAgentPartial(
-    "assets/reefscapevid.mp4",
+    FileCapture(CommonVideos.ReefscapeCompilation.path),
     None,
     InferenceMode.ALCOROULTRALYTICSSMALL2025BAD,
     showFrames=True,
