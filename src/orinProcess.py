e49190af05f94a303ef1932f4e34a2a80d99d48c
Agent Smith <488matrix@gmail.com>
2025-03-20 12:49:06 -0700

    only using pathing. less computation until we get everything actually working

--- src/orinProcess.py
+++ src/orinProcess.py
@@ -1,13 +1,11 @@
 from Core.Neo import Neo
-from Core.Agents.CentralAgent import CentralAgent
-
 from Core.Agents.PathToNearestCoralStation import PathToNearestCoralStation
 
 n = Neo()
 
-central = n.getCentral()
-
-n.wakeAgent(CentralAgent, isMainThread=False)
+# central = n.getCentral()
+#
+# n.wakeAgent(CentralAgent, isMainThread=False)
 n.wakeAgent(PathToNearestCoralStation, isMainThread=False)
 
 # start pathplanning rpc
