e3244908285e325b135e5af0ea2f3145655e10b2
AdemCan <ademfcan@gmail.com>
2025-03-29 14:03:09 -0700

    add reef provider

--- src/Alignment/ReefPostAlignmentProvider.py
+++ src/Alignment/ReefPostAlignmentProvider.py
@@ -12,9 +12,14 @@ class ReefPostAlignmentProvider(AlignmentProvider):
         super().__init__()
 
     def create(self):
+        super().create()
         self.hist, self.mtime = staticLoad(
-            "assets/stingerHistogram.npy", isRelativeToSource=True
+            "assets\stingerHistogram.npy", isRelativeToSource=True
         )
+        self.threshold = self.propertyOperator.createProperty("Reef_Post_Threshold", 60)
+        self.erosioniter = self.propertyOperator.createProperty("Reef_Erosion_iter", 1)
+        self.dilationiter = self.propertyOperator.createProperty("Reef_Dilation_iter", 2)
+        self.kernelSize = self.propertyOperator.createProperty("SquareKernelLength",3)
 
     def isColorBased(self):
         return True  # HAS to be color
@@ -27,62 +32,37 @@ class ReefPostAlignmentProvider(AlignmentProvider):
         cut1 = fourth
         cut2 = 3*fourth
 
-        frameClipped = frame[:,cut1:cut2]
+        frame = frame[:,cut1:cut2] #clip roi
 
     
 
-        midFrame = frame.shape[1] // 2
         lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         backProj = cv2.calcBackProject([lab], [1, 2], self.hist, [0, 256, 0, 256], 1)
 
-        # left side ========
-        left = -1
 
-        # extract left side of frame
-        backProj_left = backProj[:, :midFrame]
-
-        _, thresh = cv2.threshold(backProj_left, 80, 255, type=cv2.THRESH_BINARY)
-        erode_big = cv2.erode(thresh, np.ones((3, 3)), iterations=10)
-        dil_big = cv2.dilate(erode_big, np.ones((3, 3)), iterations=10)
+        _, thresh = cv2.threshold(backProj, self.threshold.get(), 255, type=cv2.THRESH_BINARY)
+        ksize = self.kernelSize.get()
+        erode_big = cv2.erode(thresh, np.ones((ksize, ksize)), iterations=self.erosioniter.get())
+        dil_big = cv2.dilate(erode_big, np.ones((ksize, ksize)), iterations=self.dilationiter.get())
 
         contours, _ = cv2.findContours(
             dil_big, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
         )
+
+        left, right, m = None, None, None
         if contours:
             biggest = max(contours, key=lambda c: cv2.boundingRect(c)[3])
             if draw:
                 cv2.drawContours(frame, [biggest], -1, (0, 0, 255), 2)
             x, y, w, h = cv2.boundingRect(biggest)
-            left = abs(x - midFrame)
 
-        # right side ========
-        right = -1
-
-        # extract right side of frame
-        backProj_right = backProj[:, midFrame:]
-
-        _, thresh = cv2.threshold(backProj_right, 30, 255, type=cv2.THRESH_BINARY)
-        erode_big = cv2.erode(thresh, np.ones((3, 3)), iterations=3)
-        dil_big = cv2.dilate(erode_big, np.ones((3, 3)), iterations=5)
-
-        contours, _ = cv2.findContours(
-            dil_big, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
-        )
-        if contours:
-            biggest = max(contours, key=lambda c: cv2.boundingRect(c)[3])
-            shifted_biggest = biggest + np.array([midFrame, 0])
-            if draw:
-                cv2.drawContours(frame, [shifted_biggest], -1, (0, 0, 255), 2)
-            x, y, w, h = cv2.boundingRect(shifted_biggest)
-            right = abs(x - midFrame)
+            left = x
+            right = x+w
+            m = (left+right)/2
+            
 
         if draw:
-            cv2.putText(frame, f"L: {left} R: {right}", (10, 20), 1, 1, (255))
-
-            cv2.imshow("gray",gray)
-            cv2.imshow("roi", frameClipped)
-
+            cv2.putText(frame, f"L: {left} R: {right} M: {m}", (10, 20), 1, 1, (255))
 
         return left, right
