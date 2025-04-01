1ef2694393653b70346af153d43ab7d9b182e09d
Ademfcan <ademfcan@gmail.com>
2025-04-01 02:00:38 -0700

    push updates

--- src/Alignment/ReefPostAlignmentProviderBlobs.py
+++ src/Alignment/ReefPostAlignmentProviderBlobs.py
@@ -19,20 +19,18 @@ class ReefPostAlignmentProvider(AlignmentProvider):
 
         # Load precomputed color histogram
         self.hist, self.mtime = staticLoad(
-            "assets/histograms/reef_post_hist.npy",
-            isRelativeToSource=True
-            # "assets/s_reef_hist.npy", isRelativeToSource=True
+            "assets/histograms/reef_post_hist.npy", isRelativeToSource=True
         )
 
         # Adjustable processing parameters
-        self.solidity = self.propertyOperator.createProperty("solidity", 0.3)
+        self.solidity = self.propertyOperator.createProperty("solidity", 0.8)
         self.threshold_pre = self.propertyOperator.createProperty(
-            "backproject_threshold", 150
+            "backproject_threshold", 30
         )
         self.roi_fraction = self.propertyOperator.createProperty("ROI_Fraction", 0.5)
-        self.ksize = self.propertyOperator.createProperty("ksize", 3)
+        self.ksize = self.propertyOperator.createProperty("ksize", 7)
         self.erosionIter = self.propertyOperator.createProperty(
-            "Hughlines_pre_erosion", 10
+            "Hughlines_pre_erosion", 2
         )
 
         # Read-only property for histogram update time
@@ -87,8 +85,9 @@ class ReefPostAlignmentProvider(AlignmentProvider):
         )
 
         # Black out area blocked by stinger for testing
-        cv2.rectangle(eroded, (0, 150), (eroded.shape[1], 200), (0), -1)
-        cv2.rectangle(frame, (0, 150), (frame.shape[1], 200), (0, 0, 0), -1)
+        if draw:
+            cv2.rectangle(eroded, (0, 150), (eroded.shape[1], 200), (0), -1)
+            cv2.rectangle(frame, (0, 150), (frame.shape[1], 200), (0, 0, 0), -1)
 
         # Find contours
         contours, _ = cv2.findContours(
@@ -102,42 +101,60 @@ class ReefPostAlignmentProvider(AlignmentProvider):
         largest_blob = None
         max_width = 0
 
+        bigmask = np.zeros_like(threshold_pre, dtype=np.uint8)
+
         for contour in contours:
             rotated_rect = cv2.minAreaRect(contour)  # Get rotated rectangle
+            (_, (w, h), _) = rotated_rect
+            totalArea = w * h
             box = cv2.boxPoints(rotated_rect)
             box = np.int0(box)  # Convert to int
 
+            mask = np.zeros_like(threshold_pre, dtype=np.uint8)
+            cv2.fillPoly(mask, [box], (255))
+
+            maskArea = np.count_nonzero(cv2.bitwise_and(mask, threshold_pre))
+
+            bigmask = cv2.bitwise_or(bigmask, mask)
+
             width = min(rotated_rect[1])  # Get width and height of rotated rect
 
-            hull = cv2.convexHull(contour)
-            area = cv2.contourArea(contour)
-            hull_area = cv2.contourArea(hull)
-
-            if hull_area > 0:
-                solidity = area / hull_area  # Solidity close to 1 means fewer branches
-                cv2.putText(
-                    eroded,
-                    f"{solidity:.1f}",
-                    (
-                        int(rotated_rect[0][0] + rotated_rect[1][0]),
-                        int(rotated_rect[0][1]),
-                    ),
-                    1,
-                    1,
-                    (255),
-                    1,
-                )
+            if totalArea > 0:
+                solidity = (
+                    maskArea / totalArea
+                )  # Solidity close to 1 means fewer branches
+                if draw:
+                    cv2.putText(
+                        eroded,
+                        f"{solidity:.1f}",
+                        (
+                            int(rotated_rect[0][0] + rotated_rect[1][0]),
+                            int(rotated_rect[0][1]),
+                        ),
+                        1,
+                        1,
+                        (255),
+                        1,
+                    )
 
                 # Ensure blob is wide, not on the side, and has a high solidity
                 if width > max_width and solidity > self.solidity.get():
                     max_width = width
                     largest_blob = box
 
+        leftright = (None, None)
         # Draw the selected blob as a rotated rectangle
         if largest_blob is not None:
-            cv2.drawContours(
-                frame, [largest_blob + (left_bound, 0)], -1, (0, 255, 0), 2
-            )
+            centroid = np.mean(largest_blob, axis=0)
 
-        cv2.imshow("Eroded", eroded)
-        return (None, None)
+            leftright = (
+                int(centroid[0] + left_bound),
+                int(centroid[0] + left_bound),
+            )  # since we are only taking the centroid, left and right will be the x
+
+            if draw:
+                cv2.drawContours(
+                    frame, [largest_blob + (left_bound, 0)], -1, (0, 255, 0), 2
+                )
+
+        return leftright
--- src/Core/Agents/Partials/AlignmentProviderAgent.py
+++ src/Core/Agents/Partials/AlignmentProviderAgent.py
@@ -29,29 +29,29 @@ class AlignmentProviderAgent(CameraUsingAgentBase):
         self.alignmentProvider._inject(self.propertyOperator)
         self.alignmentProvider.create()
 
-        # self.leftDistanceProp = self.propertyOperator.createCustomReadOnlyProperty(
-        #     propertyTable="verticalEdgeLeftDistancePx",
-        #     propertyValue=-1,
-        #     addBasePrefix=True,
-        #     addOperatorPrefix=False,
-        # )
-        # self.rightDistanceProp = self.propertyOperator.createCustomReadOnlyProperty(
-        #     propertyTable="verticalEdgeRightDistancePx",
-        #     propertyValue=-1,
-        #     addBasePrefix=True,
-        #     addOperatorPrefix=False,
-        # )
-
         self.leftDistanceProp = self.propertyOperator.createCustomReadOnlyProperty(
-            propertyTable="photonvisionfrontleft.verticalEdgeLeftDistancePx",
+            propertyTable="verticalEdgeLeftDistancePx",
             propertyValue=-1,
-            addBasePrefix=False,
+            addBasePrefix=True,
             addOperatorPrefix=False,
         )
         self.rightDistanceProp = self.propertyOperator.createCustomReadOnlyProperty(
-            propertyTable="photonvisionfrontleft.verticalEdgeRightDistancePx",
+            propertyTable="verticalEdgeRightDistancePx",
             propertyValue=-1,
-            addBasePrefix=False,
+            addBasePrefix=True,
+            addOperatorPrefix=False,
+        )
+
+        self.hresProp = self.propertyOperator.createCustomReadOnlyProperty(
+            propertyTable="cameraHres",
+            propertyValue=640,
+            addBasePrefix=True,
+            addOperatorPrefix=False,
+        )
+        self.vresProp = self.propertyOperator.createCustomReadOnlyProperty(
+            propertyTable="cameraVres",
+            propertyValue=480,
+            addBasePrefix=True,
             addOperatorPrefix=False,
         )
 
@@ -63,6 +63,8 @@ class AlignmentProviderAgent(CameraUsingAgentBase):
         )
         self.leftDistanceProp.set(left)
         self.rightDistanceProp.set(right)
+        self.hresProp.set(frame.shape[1])
+        self.vresProp.set(frame.shape[0])
 
     def getDescription(self) -> str:
         return "Looks-Through-Camera-Checks-Alignment"
