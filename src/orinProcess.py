ec570823c4ca6715afb8558fb712b2f8d3f0f260
Agent Smith <488matrix@gmail.com>
2025-04-03 13:39:56 -0700

    fix agents not working

--- src/orinProcess.py
+++ src/orinProcess.py
@@ -1,34 +1,12 @@
-from Core.Neo import Neo
-from Core.Agents.PathToNearestCoralStation import PathToNearestCoralStation
-from Core.Agents.PathToNearestBarge import PathToNearestBarge
+from multiprocessing import Process
 
-if __name__ == "__main__":
+import PathToNearestCoralStation
+import PathToNearestBarge
 
-    # import signal
-    # signal.signal(signal.SIGINT, signal.SIG_DFL)
-    #
-    n = Neo()
-    #
-    # n.wakeAgent(CentralAgent, isMainThread=False)
-    n.wakeAgent(PathToNearestCoralStation, isMainThread=False)
-    n.wakeAgent(PathToNearestBarge, isMainThread=False)
+p2 = Process(target=PathToNearestBarge.start)
+p1 = Process(target=PathToNearestCoralStation.start)
 
-    #
-    # object_localization = ObjectLocalizingAgentPartial(
-    #     inferenceMode=InferenceMode.ALCOROBEST2025GPUONLY,
-    #     capture=D435Capture(res=D435IResolution.RS720P),
-    #     cameraExtrinsics=ColorCameraExtrinsics2025.DEPTH_REAR_LEFT,
-    # )
-
-    # n.wakeAgent(object_localization, isMainThread=False)
-
-    # n.wakeAgent(orinIngestorAgent,isMainThread=False)
-    # n.wakeAgent(
-    #     partialVideoWriterAgent(FileCapture(0), savePath=f"orinCam_{getTimeStr()}.mp4"),
-    #     isMainThread=False,
-    # )
-
-    # start pathplanning rpc
-    from pathplanning.nmc import fastMarchingMethodRPC
-
-    fastMarchingMethodRPC.serve()
+p1.start()
+p2.start()
+p1.join()
+p2.join()
