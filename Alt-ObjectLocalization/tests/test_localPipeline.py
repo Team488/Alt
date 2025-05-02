187a6802dcc21549f7d936a9e3abd8b68e72b9b3
AdemCan <ademfcan@gmail.com>
2025-05-02 04:21:17 -0700

    update readme, small updates to tests

--- Alt-ObjectLocalization/tests/test_localPipeline.py
+++ Alt-ObjectLocalization/tests/test_localPipeline.py
@@ -1,8 +1,8 @@
-from Alt.ObjectLocalization.Localization.PipelineStep1 import PipelineStep1
-from Alt.ObjectLocalization.Localization.PipelineStep2 import PipelineStep2
+# from Alt.ObjectLocalization.Localization.PipelineStep1 import PipelineStep1
+# from Alt.ObjectLocalization.Localization.PipelineStep2 import PipelineStep2
 
-from Alt.ObjectLocalization.Inference.ModelConfig import ModelConfig
+# from Alt.ObjectLocalization.Inference.ModelConfig import ModelConfig
 
-def test_Step1():
-    config = ModelConfig()
-    step1 = PipelineStep1()
+# def test_Step1():
+#     config = ModelConfig()
+#     step1 = PipelineStep1()
