4623707cd00d027bed63b541700ef96c3b8f471c
AdemCan <ademfcan@gmail.com>
2025-05-04 02:35:24 -0700

    nuke everything 'competition or device specific' as this will be the general repo, for now files have been moved to Alt-Reefscape where they will be filtered and remain there

--- assets/mars-small128.pb
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:621bbebe9a811b318129da2a099d631ffa33351d9974343e39820be520943c21
-size 11244842
--- demos/BaseDemo.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Demo(ABC):
-    @abstractmethod
-    def startDemo(self) -> None:
-        # every demo will have a start demo
-        pass
